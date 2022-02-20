a496715210087ccc68ba36f6f9da2f0999834e8c
silvester <silvesterlhwali123@gmail.com>
2022-02-20 21:31:41 +0800

    feat: add documentation for employee position controller

--- app/Http/Controllers/EmployeePositionController.php
+++ app/Http/Controllers/EmployeePositionController.php
@@ -9,7 +9,21 @@ use App\Http\Requests\UpdateEmployeePositionRequest;
 class EmployeePositionController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *   tags={"EmployeePosition"},
+     *   path="/api/v1/employee-positions",
+     *   summary="Display a listing of the employee positions resource",
+     *   security={{"sanctum ":{}}},
+     *   @OA\Response(response=200, description="OK",
+     *      @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref="#/components/schemas/EmployeePosition")
+     *      )
+     * ),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     * 
      *
      * @return \Illuminate\Http\Response
      */
@@ -19,7 +33,55 @@ class EmployeePositionController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *   tags={"EmployeePosition"},
+     *   path="/api/v1/employees-positions",
+     *   summary="Store a newly created resource in employee position storage",
+     *   security={{"sanctum ":{}}},
+     *   @OA\Parameter(
+     *     name="position_code",
+     *     in="query",
+     *     required=true,
+     *     description="Position code property",
+     *     @OA\Schema(type="string",example="STF")
+     *   ),
+     *   @OA\Parameter(
+     *     name="position",
+     *     in="query",
+     *     required=true,
+     *     description="Position property",
+     *     @OA\Schema(type="string",example="STF")
+     *   ),
+     *   @OA\Parameter(
+     *     name="description",
+     *     in="query",
+     *     description="description property",
+     *     @OA\Schema(type="string",example="This position is awesome")
+     *   ),
+     *   @OA\Parameter(
+     *     name="sorting_number",
+     *     in="query",
+     *     required=true,
+     *     description="sorting number  property",
+     *     @OA\Schema(type="string",example="1000")
+     *   ),
+     *   @OA\Response(response=200, description="OK",
+     *      @OA\JsonContent(
+     *        type="object",
+     *        @OA\Property(
+     *          property="message", type="string",example="Employee Position created successfully",
+     *        ),
+     *        @OA\Property(
+     *          property="data", 
+     *          type="object",
+     *          ref="#/components/schemas/EmployeePosition"
+     *        )
+     *      )
+     * ),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     * .
      *
      * @param  \App\Http\Requests\StoreEmployeePositionRequest  $request
      * @return \Illuminate\Http\Response
@@ -28,13 +90,36 @@ class EmployeePositionController extends Controller
     {
         $employeePosition = EmployeePosition::create($request->all());
         return response()->json([
-            "message" => "Success create new employee position",
+            "message" => "Employee Position created successfully",
             "data" => $employeePosition
         ]);
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *   tags={"EmployeePosition"},
+     *   path="/api/v1/employees-positions/{id}",
+     *   summary="Display the specified employee position resource.",
+     *   security={{"sanctum ":{}}},
+     *   @OA\Parameter(
+     *      name="id",
+     *      description="id the employee position table",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(
+     *          type="integer"
+     *         )
+     *      ),
+     *   @OA\Response(response=200, description="OK",
+     *      @OA\JsonContent(
+     *        type="object",
+     *        ref="#/components/schemas/EmployeePosition"
+     *      )
+     *  ),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     * 
      *
      * @param  \App\Models\EmployeePosition  $employeePosition
      * @return \Illuminate\Http\Response
@@ -46,7 +131,64 @@ class EmployeePositionController extends Controller
 
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *   tags={"EmployeePosition"},
+     *   path="/api/v1/employees-positions/{id}",
+     *   summary="Update the specified resource in employee position storage.",
+     *   security={{"sanctum ":{}}},
+     *   @OA\Parameter(
+     *      name="id",
+     *      description="id the employee position table",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(
+     *          type="integer"
+     *         )
+     *      ),
+     *   @OA\Parameter(
+     *     name="position_code",
+     *     in="query",
+     *     required=true,
+     *     description="Position code property",
+     *     @OA\Schema(type="string",example="STF")
+     *   ),
+     *   @OA\Parameter(
+     *     name="position",
+     *     in="query",
+     *     required=true,
+     *     description="Position property",
+     *     @OA\Schema(type="string",example="STF")
+     *   ),
+     *   @OA\Parameter(
+     *     name="description",
+     *     in="query",
+     *     description="description property",
+     *     @OA\Schema(type="string",example="This position is awesome")
+     *   ),
+     *   @OA\Parameter(
+     *     name="sorting_number",
+     *     in="query",
+     *     required=true,
+     *     description="sorting number  property",
+     *     @OA\Schema(type="string",example="1000")
+     *   ),
+     *   @OA\Response(response=200, description="OK",
+     *      @OA\JsonContent(
+     *        type="object",
+     *        @OA\Property(
+     *          property="message", type="string",example="Employee position updated successfully",
+     *        ),
+     *        @OA\Property(
+     *          property="data", 
+     *          type="object",
+     *          ref="#/components/schemas/EmployeePosition"
+     *        )
+     *      )
+     * ),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
+     * 
      *
      * @param  \App\Http\Requests\UpdateEmployeePositionRequest  $request
      * @param  \App\Models\EmployeePosition  $employeePosition
@@ -63,8 +205,38 @@ class EmployeePositionController extends Controller
         );
     }
 
+
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *   tags={"EmployeePosition"},
+     *   path="/api/v1/employees-positions/{id}",
+     *   summary="Remove the specified resource from employee position storage.",
+     *   security={{"sanctum ":{}}},
+     *   @OA\Parameter(
+     *      name="id",
+     *      description="id the employee position table",
+     *      required=true,
+     *      in="path",
+     *      @OA\Schema(
+     *          type="integer"
+     *         )
+     *      ),
+     *   @OA\Response(response=200, description="OK",
+     *      @OA\JsonContent(
+     *        type="object",
+     *        @OA\Property(
+     *          property="message", type="string",example="Employee position deleted successfully",
+     *        ),
+     *        @OA\Property(
+     *          property="data", 
+     *          type="object",
+     *          ref="#/components/schemas/EmployeePosition"
+     *        )
+     *      )
+     * ),
+     *   @OA\Response(response=401, description="Unauthorized"),
+     *   @OA\Response(response=404, description="Not Found")
+     * )
      *
      * @param  \App\Models\EmployeePosition  $employeePosition
      * @return \Illuminate\Http\Response
