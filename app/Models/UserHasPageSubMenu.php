41e8afe1fba1d100488433e2ef6eb48d3f1ee512
silvester <silvesterlhwali123@gmail.com>
2022-02-22 15:29:58 +0800

    refactor: unnknow what is privot

--- app/Models/UserHasPageSubMenu.php
+++ app/Models/UserHasPageSubMenu.php
@@ -17,7 +17,7 @@ class UserHasPageSubMenu extends Model
         return $this->belongsTo(PageSubMenu::class, 'id', 'page_sub_menu_id');
     }
 
-    public function users()
+    public function user()
     {
         return $this->belongsTo(Users::class, 'id', 'user_id');
     }
